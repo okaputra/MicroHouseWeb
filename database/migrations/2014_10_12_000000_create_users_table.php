9146647817bfcc147238aaf743fa87dadd8f2a01
okaputra <54925385+okaputra@users.noreply.github.com>
2020-03-21 20:48:33 +0800

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('roles')->default('officer');
+            $table->string('roles')->default('applicant');
             $table->rememberToken();
             $table->timestamps();
         });
