cb034ae7403a52eaf4b4b675748c5879b38a9284
okaputra <54925385+okaputra@users.noreply.github.com>
2020-03-21 21:19:55 +0800

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
+        'applicant' => \App\Http\Middleware\Applicant::class,
+        'officer' => \App\Http\Middleware\Officer::class,
     ];
 }
