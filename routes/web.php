bf9758f326705c7cb29943e95f79941fe935f800
okaputra <54925385+okaputra@users.noreply.github.com>
2020-04-01 20:22:38 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -25,21 +25,25 @@ Route::get('/', 'AuthController@getLogin')->middleware('guest')->name('login');
 Route::post('/', 'AuthController@postLogin');
 Route::get('/register','AuthController@getRegister')->name('register');
 Route::post('/register','AuthController@postRegister')->name('register')->middleware('guest');
-Route::post('/logout','AuthController@logOut')->middleware('auth')->name('logout');
 
-//routes for residence
-Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
-Route::get('/rent', 'HousingController@index')->middleware('auth');
-Route::post('/storeNewR', 'HousingController@store')->middleware('auth');
-Route::get('/delete/{id}', 'HousingController@destroy')->middleware('auth');
-Route::get('/editResidence/{id}', 'HousingController@edit')->middleware('auth');
-Route::get('/showDetail/{id}', 'HousingController@show')->middleware('auth');
-Route::post('/updateResidence', 'HousingController@update')->middleware('auth');
+Route::group(['middleware' => 'auth'],function(){
+    
+    //routes for residence
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::get('/rent', 'HousingController@index');
+    Route::post('/storeNewR', 'HousingController@store');
+    Route::get('/delete/{id}', 'HousingController@destroy');
+    Route::get('/editResidence/{id}', 'HousingController@edit');
+    Route::get('/showDetail/{id}', 'HousingController@show');
+    Route::post('/updateResidence', 'HousingController@update');
+    
+    //routes for application
+    Route::get('/application','ApplicationController@index');
+    Route::post('/submitApplication','ApplicationController@store');
+    Route::get('/apkDetail/{id}','ApplicationController@show');
+    Route::get('/deleteApk/{id}','ApplicationController@destroy');
+    Route::get('/editApplication/{id}', 'ApplicationController@edit');
+    Route::post('/updateApplication', 'ApplicationController@update');
+    Route::post('/logout','AuthController@logOut')->name('logout');
+});
 
-//routes for application
-Route::get('/application','ApplicationController@index')->middleware('auth');
-Route::post('/submitApplication','ApplicationController@store')->middleware('auth');
-Route::get('/apkDetail/{id}','ApplicationController@show')->middleware('auth');
-Route::get('/deleteApk/{id}','ApplicationController@destroy')->middleware('auth');
-Route::get('/editApplication/{id}', 'ApplicationController@edit')->middleware('auth');
-Route::post('/updateApplication', 'ApplicationController@update')->middleware('auth');
